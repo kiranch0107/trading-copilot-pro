8500c1a136a9128e9a47b8378a9b3e8b64a746d5
kiranch0107 <kiranch0107@gmail.com>
2026-08-28 12:35:56 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -55,7 +55,7 @@ import requests
 import ta
 import yfinance as yf
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
+logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO").upper(),format="%(asctime)s %(levelname)s: %(message)s")
 logger = logging.getLogger("scanner")
 
 ET = pytz.timezone("America/New_York")
